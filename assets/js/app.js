a9b56075636d50776aeb605dae959a709d2a7830
Ewen <ewen.herve.35550@gmail.com>
2022-04-14 13:55:53 +0200

    New Portfolio

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,89 +1,142 @@
-$('.slider').each(function() {
-    var $this = $(this);
-    var $group = $this.find('.slide_group');
-    var $slides = $this.find('.slide');
-    var bulletArray = [];
-    var currentIndex = 0;
-    var timeout;
-    
-    function move(newIndex) {
-        var animateLeft, slideLeft;
+/* ==================================================
+   ===================== LOADER =====================
+   ================================================== */ 
 
-        advance();
+$(window).on('load', () => {$('.loader').addClass("loaded");});
 
-        if ($group.is(':animated') || currentIndex === newIndex) {
-            return;
-        }
-
-        bulletArray[currentIndex].removeClass('active');
-        bulletArray[newIndex].addClass('active');
-
-        if (newIndex > currentIndex) {
-            slideLeft = '100%';
-            animateLeft = '-100%';
-        } else {
-            slideLeft = '-100%';
-            animateLeft = '100%';
-        }
-
-        $slides.eq(newIndex).css({
-            display: 'flex',
-            left: slideLeft
-        });
-        $group.animate({
-            left: animateLeft
-        }, function() {
-            $slides.eq(currentIndex).css({
-                display: 'none'
-            });
-            $slides.eq(newIndex).css({
-                left: 0
-            });
-            $group.css({
-                left: 0
-            });
-            currentIndex = newIndex;
-        });
+/* ==================================================
+   ====================== NAV =======================
+   ================================================== */ 
+function navbar_bg(){
+    if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
+        $('.navbar').css('background-color', '#1D1D22');
+        $('.navbar').css('box-shadow', '0px 10px 15px -10px #343439');
+    } else {
+        $('.navbar').css('background-color', 'transparent');
+        $('.navbar').css('box-shadow', 'none');
     }
+}
 
-    function advance() {
-        clearTimeout(timeout);
-        timeout = setTimeout(function() {
-            if (currentIndex < ($slides.length - 1)) {
-                move(currentIndex + 1);
-            } else {
-                move(0);
+$(window).on('load', () => {navbar_bg();});
+$(window).on('scroll', () => {navbar_bg();});
+
+$('#btn-nav-hamburger').on('click', () => {
+    $('.hamburger_bar').toggleClass('active_btn_hamburger');
+    $('.nav_container').toggleClass('active_nav');
+});
+
+/* ==================================================
+   ====================== NAV =======================
+   ================================================== */ 
+
+const home = $('#Accueil');
+const bts = $('#BTS');
+const about = $('#About');
+const projets = $('#Projets');
+const contact = $('#Contact');
+
+const navbar = $('.navbar');
+
+$('.link-home').on('click', function () {$(window).scrollTop(home.offset().top - navbar.innerHeight());});
+$('.link-bts').on('click', function () {$(window).scrollTop(bts.offset().top - (navbar.innerHeight() / 2));});
+$('.link-about').on('click', function () {$(window).scrollTop(about.offset().top - (navbar.innerHeight() / 2));});
+$('.link-projets').on('click', function () {$(window).scrollTop(projets.offset().top - (navbar.innerHeight() * 1.5));});
+$('.link-contact').on('click', function () {$(window).scrollTop(contact.offset().top - (navbar.innerHeight() / 2));});
+
+
+function navigation(){
+    if (home.offset().top <= $(window).scrollTop() && $(window).scrollTop() < home.innerHeight()){
+        $('.link-home').addClass('active');
+        $('.link-bts').removeClass('active');
+        $('.link-about').removeClass('active');
+        $('.link-projets').removeClass('active');
+        $('.link-contact').removeClass('active');
+    }
+    if (bts.offset().top - (navbar.innerHeight() / 2) - 1 <= $(window).scrollTop() && $(window).scrollTop() < (bts.offset().top + bts.innerHeight())){
+        $('.link-home').removeClass('active');
+        $('.link-bts').addClass('active');
+        $('.link-about').removeClass('active');
+        $('.link-projets').removeClass('active');
+        $('.link-contact').removeClass('active');
+    }
+    if (about.offset().top - (navbar.innerHeight() / 2) - 1 <= $(window).scrollTop() && $(window).scrollTop() < (about.offset().top + about.innerHeight())){
+        $('.link-home').removeClass('active');
+        $('.link-bts').removeClass('active');
+        $('.link-about').addClass('active');
+        $('.link-projets').removeClass('active');
+        $('.link-contact').removeClass('active');
+    }
+    if (projets.offset().top - (navbar.innerHeight() * 1.5) - 1 <= $(window).scrollTop() && $(window).scrollTop() < (projets.offset().top + projets.innerHeight())){
+        $('.link-home').removeClass('active');
+        $('.link-bts').removeClass('active');
+        $('.link-about').removeClass('active');
+        $('.link-projets').addClass('active');
+        $('.link-contact').removeClass('active');
+    }
+    if (contact.offset().top <= $(window).scrollTop() && $(window).scrollTop() < (contact.offset().top + contact.innerHeight())){
+        $('.link-home').removeClass('active');
+        $('.link-bts').removeClass('active');
+        $('.link-about').removeClass('active');
+        $('.link-projets').removeClass('active');
+        $('.link-contact').addClass('active');
+    }
+}
+
+$(window).on('load', function(){navigation();});
+$(window).on('scroll', function(){navigation();});
+
+/* ==================================================
+   ===================== SLIDE ======================
+   ================================================== */ 
+
+$('#slide_slam').on('click', () => {
+    $('.options.sisr').css("transform", "translateX(-110%)");
+    $('.options.slam').css("transform", "translateX(0)");
+});
+
+$('#slide_sisr').on('click', () => {
+    $('.options.sisr').css("transform", "translateX(0)");
+    $('.options.slam').css("transform", "translateX(110%)");
+});
+
+/* ==================================================
+   ===================== MODALE =====================
+   ================================================== */ 
+
+const buttonModales = $('.open_modale');
+
+buttonModales.each((e) => {
+    buttonModales[e].addEventListener('click', (response) => {
+        const modales = $('#Modales').children();
+
+        modales.each((i) => { 
+            if (modales[i].classList.contains(response.target.parentNode.classList[1])){
+                document.querySelector('#Modales').classList.add('active');
+                modales[i].classList.add('active');
             }
-        }, 5000);
-    }
+        });
 
-    $('.next_btn').on('click', function() {
-        if (currentIndex < ($slides.length - 1)) {
-            move(currentIndex + 1);
-        } else {
-            move(0);
-        }
     });
+});
 
-    $('.previous_btn').on('click', function() {
-        if (currentIndex !== 0) {
-            move(currentIndex - 1);
-        } else {
-            move($slides.length - 1);
-        }
+const buttonClose = $('.fa-xmark');
+
+buttonClose.each((e) => {
+    buttonClose[e].addEventListener('click', () => {
+        const modales = $('#Modales').children();
+
+        modales.each(() => { 
+            document.querySelector('#Modales').classList.remove('active');
+            modales[e].classList.remove('active');
+        });
     });
+});
 
-    $.each($slides, function(index) {
-        var $button = $('<a class="slide_btn">&bull;</a>');
+$('#Modales').on('click', () => {
+    const modales = $('#Modales').children();
 
-        if (index === currentIndex) {
-            $button.addClass('active');
-        }
-        $button.on('click', function() {
-            move(index);
-        }).appendTo('.slide_buttons');
-        bulletArray.push($button);
+    modales.each((e) => {
+        document.querySelector('#Modales').classList.remove('active');
+        modales[e].classList.remove('active');
     });
-
-    advance();
 });
